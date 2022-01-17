38b10eb848944e5ccb3d08ae8bda2b9749c353a2
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-01-17 09:26:50 +0530

    Time: 21 ms (28.32%), Space: 13.4 MB (11.30%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+       int m=matrix.size();
+        int n=matrix[0].size();
+        map<int,int> a,b;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    a[i]++;
+                    b[j]++;
+                }
+                
+            }
+        }
+        
+        for(auto it=a.begin();it!=a.end();it++)
+        {
+            int x=it->first;
+            
+            for(int i=0;i<n;i++)
+                matrix[x][i]=0;
+        }
+        
+        for(auto it=b.begin();it!=b.end();it++)
+        {
+            int x=it->first;
+            
+            for(int i=0;i<m;i++)
+                matrix[i][x]=0;
+        }
+    }
+};
