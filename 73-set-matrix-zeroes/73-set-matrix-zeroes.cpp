aa6b051c3cf9ee38bf5991701375c9d9d34144a2
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-01-17 09:27:35 +0530

    Time: 21 ms (28.32%), Space: 13.4 MB (19.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         
        int m=matrix.size();
         int n=matrix[0].size();
-        map<int,int> a,b;
+        unordered_map<int,int> a,b;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
