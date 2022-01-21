d493a8c65d18f69ad2225882caa26a30667d7946
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-01-21 21:08:15 +0530

    Time: 8 ms (23.75%), Space: 9.4 MB (95.52%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -7,20 +7,25 @@ public:
         
         bool f=false;
         
-        for(int i=0;i<m;i++)
+        int lb=0,ub=n*m-1;
+        
+        while(lb<=ub)
         {
-            if(target<matrix[i][0]||target>matrix[i][n-1])
-                continue;
-            int v=lower_bound(matrix[i].begin(),matrix[i].end(),target)-matrix[i].begin();
-            if(v<n&&matrix[i][v]==target)
+            int mi=(lb+ub)/2;
+            int r=mi/n;
+            int c=mi%n;
+            
+            if(target<matrix[r][c])
+                ub=mi-1;
+            else if(target>matrix[r][c])
+                lb=mi+1;
+            else
             {
                 f=true;
                 break;
             }
-            
         }
         
         return f;
-        
     }
 };
