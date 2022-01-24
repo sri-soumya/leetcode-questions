f561505dece75d5020730dbf6deb329de5078b2d
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-01-24 14:10:20 +0530

    Time: 42 ms (24.73%), Space: 8.6 MB (42.28%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,7 +4,7 @@ public:
         
         int n=s.length();
         map<char,int> x;
-        int c=0,m=0;
+        /*int c=0,m=0;
         for(int i=0;i<n;i++)
         {
             if(x.count(s[i])==0)
@@ -14,6 +14,26 @@ public:
             m=max(m,c);
             //cout<<m<<endl;
             x[s[i]]=i;
+        }
+        
+        return m;*/
+        
+        //same concept but the below code is more understandable
+        
+        int l=0,r=0,m=0;
+        
+        while(r<n)
+        {
+            
+            if(x.count(s[r])==0)
+                r++;
+            else if(x[s[r]]<l)
+                r++;
+            else
+                l=x[s[r]]+1;
+            x[s[r-1]]=r-1;
+            m=max(m,r-l);
+            //cout<<l<<" "<<r<<endl;
         }
         
         return m;
