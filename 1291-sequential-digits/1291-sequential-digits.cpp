fc1e069adfe2579be58e9fe222b66bde49359339
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-01-25 13:52:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (90.56%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,56 +1,23 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
+        
         vector<int> ans;
-        string l=to_string(low),h=to_string(high);
-        //cout<<l<<endl;
-        string s=l.substr(0,1);
-        //cout<<s<<endl;
-        while(s.length()<l.length())
+        
+        for(int i=1;i<=9;i++)
         {
-            int n=s.length();
-            int c=s[n-1]-'0';
-            
-            if(c<9)
+            int k=i;
+            for(int j=i+1;j<=9;j++)
             {
-                
-                s+=to_string((c+1));
-                //cout<<s<<endl;
+                k=k*10+j;
+                if(k>=low&&k<=high)
+                    ans.push_back(k);
             }
-            
-            else
-            {
-                s="";
-                for(int i=1;i<=l.size()+1;i++)
-                    s+=to_string(i);
-            }
-        }
-        cout<<s<<endl;
-        while(stol(s)<=high)
-        {
-            
-            if(stol(s)>=low)
-                ans.push_back(stol(s));
-            
-            //cout<<"here"<<endl;
-            int n=s.length();
-            int c=s[n-1]-'0';
-            
-            if(c<9)
-                s=s.substr(1)+to_string(c+1);
-            else
-            {
-                string z="";
-                for(int i=1;i<=n+1;i++)
-                    z+=to_string(i);
-                s=z;
-            }
-            
-            
         }
         
-        cout<<s<<endl;
+        sort(ans.begin(),ans.end());
         
         return ans;
+        
     }
 };
