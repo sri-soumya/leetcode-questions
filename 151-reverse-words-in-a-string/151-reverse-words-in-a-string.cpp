ec57d47e0da7ebf834eab4b3ad9290cb14b98935
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-01-27 10:36:21 +0530

    Time: 24 ms (9.63%), Space: 20.7 MB (28.46%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,50 +1,30 @@
 class Solution {
 public:
-    
-    void rev(string &s,int b,int n)
-    {
-        n--;
-        while(b<n)
-        {
-            swap(s[b],s[n]);
-            b++;n--;
-        }
-    }
-    
-    
     string reverseWords(string s) {
         
-        s+=" ";
-        int l=0;
+        string t="",ans="";
+        
         for(int i=0;i<s.length();i++)
         {
-            if(s[i]==' ')
+            if(s[i]!=' ')
+                t+=s[i];
+            else
             {
-                rev(s,l,i);
-                l=i+1;
-                int c=i;
-                while(c<s.length()&&s[c]==' ')
-                {
-                    c++;
-                }
-                s=s.substr(0,i+1)+((c<s.length())?s.substr(c):"");
-                // while(i<s.length()&&s[i]==' ')
-                // {
-                //     i++;
-                // }
+                if(t.length()==0)
+                    continue;
+                if(ans.length()>0)
+                    ans=t+" "+ans;
+                else
+                    ans=t+ans;
+                t="";
             }
-            
-            
         }
+        if(t.length()&&ans.length())
+            ans=t+" "+ans;
+        else if(t.length()&&!ans.length())
+            ans=t+ans;
         
-        reverse(s.begin(),s.end());
-        
-        if(s[0]==' ')
-            s=s.substr(1);
-        if(s[s.length()-1]==' ')
-            s=s.substr(0,s.length()-1);
-        
-        return s;
+        return ans;
         
     }
 };
