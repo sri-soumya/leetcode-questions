70e9e56c07f3e23a5089f47cf08061938483fc8d
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-02-17 13:06:41 +0530

    Time: 388 ms (5.58%), Space: 159.8 MB (5.01%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     
-    vector<vector<int>> solve(vector<int> a,int t,vector<int>b,vector<vector<int>> c,int in)
+    void solve(vector<int> c,vector<vector<int>> &a,vector<int> b,int t,int in)
     {
-        if(t<=0||in>=a.size())
+        int n=c.size();
+        if(!t)
         {
-            if(t==0)
-            {
-                c.push_back(b);
-                //cout<<c.size()<<endl;
-            }
-            return c;
+            a.push_back(b);
+            return;
         }
+        if(in>=n||t<0)
+            return;
         
-        vector<vector<int>> d,e;
-        d=solve(a,t,b,c,in+1);
-        b.push_back(a[in]);
-        e=solve(a,t-a[in],b,c,in);
-        //f=solve(a,t-a[in],b,c,in+1);
-        
-        c.insert(c.end(),e.begin(),e.end());
-        //c.insert(c.end(),f.begin(),f.end());
-        c.insert(c.end(),d.begin(),d.end());
-        
-        return c;
+        solve(c,a,b,t,in+1);
+        b.push_back(c[in]);
+        solve(c,a,b,t-c[in],in);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> a;
         vector<int> b;
-        vector<vector<int>> c;
-        return solve(candidates,target,b,c,0);
+        solve(candidates,a,b,target,0);
+        return a;
     }
 };
