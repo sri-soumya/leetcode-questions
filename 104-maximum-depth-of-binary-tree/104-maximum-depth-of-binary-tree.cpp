7b3b756f8393a91057d4424492592060bb20b84e
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-02-27 22:35:29 +0530

    Time: 11 ms (63.92%), Space: 19 MB (14.59%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -12,13 +12,6 @@
 class Solution {
 public:
     
-    int height(TreeNode* root)
-    {
-        if(!root)
-            return 0;
-        return max(height(root->left),height(root->right))+1;
-    }
-    
     int solve(TreeNode* root)
     {
         if(!root)
