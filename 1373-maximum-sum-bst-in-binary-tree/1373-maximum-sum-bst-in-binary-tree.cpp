fb24ed28166cf0205a7852b856b36500ebf8d4fa
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-04-17 14:37:05 +0530

    Time: 178 ms (85.75%), Space: 103.9 MB (77.40%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -16,12 +16,13 @@ class node{
     int ma;
     int mi;
     int s;
+    int ans;
 };
 
 class Solution {
 public:
     
-    node solve(TreeNode* root,int &ans)
+    node solve(TreeNode* root)
     {
         node ob;
         if(!root)
@@ -30,24 +31,27 @@ public:
             ob.ma=INT_MIN;
             ob.mi=INT_MAX;
             ob.s=0;
+            ob.ans=0;
             return ob;
         }
         
-        auto l=solve(root->left,ans);
-        auto r=solve(root->right,ans);
+        auto l=solve(root->left);
+        auto r=solve(root->right);
         
         ob.bst=l.bst&&r.bst&&root->val>l.ma&&root->val<r.mi;
         ob.s=root->val+l.s+r.s;
         ob.mi=min(root->val,l.mi);
         ob.ma=max(root->val,r.ma);
         if(ob.bst)
-            ans=max(ans,ob.s);
+            ob.ans=max(ob.s,max(l.ans,r.ans));
+        else
+            ob.ans=max(l.ans,r.ans);
         return ob;
     }
     
     int maxSumBST(TreeNode* root) {
         int ans=0;
-        solve(root,ans);
+        return solve(root).ans;
         return ans;
     }
 };
