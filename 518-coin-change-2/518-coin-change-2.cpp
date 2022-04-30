ebf078601ad8966229f88484a7629c1ffc88e625
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-04-30 14:17:56 +0530

    Time: 60 ms (24.77%), Space: 18.4 MB (40.78%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -18,9 +18,29 @@ public:
         return dp[i][t]= solve(a,i-1,t,dp)+solve(a,i,t-a[i],dp);
     }
     
-    int change(int a, vector<int>& c) {
-        int n=c.size();
-        vector<vector<int>> dp(n,vector<int>(a+1,-1));
-        return solve(c,n-1,a,dp);
+    int change(int t, vector<int>& a) {
+        int n=a.size();
+        vector<vector<int>> dp(n,vector<int>(t+1,0));
+        //return solve(c,n-1,a,dp);
+        
+        for(int i=0;i<n;i++)
+            dp[i][0]=1;
+        
+        for(int i=1;i<=t;i++)
+            if(i%a[0]==0)
+                dp[0][i]=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=t;j++)
+            {
+                int c1=dp[i-1][j],c2=0;
+                if(j>=a[i])
+                    c2=dp[i][j-a[i]];
+                dp[i][j]=c1+c2;
+            }
+        }
+        
+        return dp[n-1][t];
     }
 };
