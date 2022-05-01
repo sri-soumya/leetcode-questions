7680b22276c633ece477f7fd660bb4172728ba64
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-05-01 23:25:31 +0530

    Time: 30 ms (50.13%), Space: 13.1 MB (19.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -41,39 +41,39 @@ public:
     
     int longestCommonSubsequence(string a, string b) {
         int n=a.length(),m=b.length();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         //return solve(a,b,n-1,m-1,dp);
         
-        if(a[0]==b[0])
-            dp[0][0]=1;
+//         if(a[0]==b[0])
+//             dp[0][0]=1;
         
-        for(int i=1;i<m;i++)
-        {
-            if(a[0]==b[i])
-                dp[0][i]=1;
-            else
-                dp[0][i]=dp[0][i-1];
-        }
+//         for(int i=1;i<m;i++)
+//         {
+//             if(a[0]==b[i])
+//                 dp[0][i]=1;
+//             else
+//                 dp[0][i]=dp[0][i-1];
+//         }
         
-        for(int i=1;i<n;i++)
-        {
-            if(a[i]==b[0])
-                dp[i][0]=1;
-            else
-                dp[i][0]=dp[i-1][0];
-        }
+//         for(int i=1;i<n;i++)
+//         {
+//             if(a[i]==b[0])
+//                 dp[i][0]=1;
+//             else
+//                 dp[i][0]=dp[i-1][0];
+//         }
         
-        for(int i=1;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<m;j++)
+            for(int j=1;j<=m;j++)
             {
-                if(a[i]==b[j])
+                if(a[i-1]==b[j-1])
                     dp[i][j]=1+dp[i-1][j-1];
                 else
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
         
-        return dp[n-1][m-1];
+        return dp[n][m];
     }
 };
