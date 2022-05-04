c9619a87dbb279f6e9e3df806227f0caeff4aaf8
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-05-04 14:07:34 +0530

    Time: 31 ms (39.55%), Space: 15.5 MB (14.78%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string a, string b) {
+     
+        int n=a.length(),m=b.length();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(a[i-1]==b[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=m;j++)
+        //         cout<<dp[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        // cout<<endl;
+        int i=n,j=m;
+        string c="";
+        while(i>0&&j>0)
+        {
+            if(dp[i][j]!=dp[i-1][j]&&dp[i][j]!=dp[i][j-1])
+            {
+                //cout<<a[i-1];
+                c=b[j-1]+c;
+                i--;
+                j--;
+            }
+            else
+            {
+                if(dp[i-1][j]>dp[i][j-1])
+                {
+                    //cout<<b[j-1];
+                    c=a[i-1]+c;
+                    i--;
+                }
+                else
+                {
+                    //cout<<a[i-1];
+                    c=b[j-1]+c;
+                    j--;
+                }
+            }
+        }
+        
+        while(i)
+        {
+            c=a[i-1]+c;
+            i--;
+        }
+        while(j)
+        {
+            c=b[j-1]+c;
+            j--;
+        }
+        
+        return c;
+        
+    }
+};
