7af7b87a2b7dd019553e1f7c4915da832132ca91
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-05-07 01:38:46 +0530

    Time: 904 ms (14.87%), Space: 207.2 MB (15.80%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -25,7 +25,27 @@ public:
     
     int maxProfit(vector<int>& a) {
         int n=a.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(a,0,1,2,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        //return solve(a,0,1,2,dp);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int f=0;f<2;f++)
+            {
+                for(int k=1;k<3;k++)
+                {
+                    if(f)
+                    {
+                        dp[i][f][k]=max(-a[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                    }
+                    else
+                    {
+                        dp[i][f][k]=max(a[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                    }
+                }
+            }
+        }
+        
+        return dp[0][1][2];
     }
 };
