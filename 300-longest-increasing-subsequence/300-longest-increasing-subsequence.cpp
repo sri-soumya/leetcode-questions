1ac18bb78e5a7d14eddbbd2b6239108ff7e09e5b
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-05-07 13:48:28 +0530

    Time: 11 ms (85.80%), Space: 10.5 MB (66.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &a,int i,int j,vector<vector<int>> &dp)
-    {
-        int n=a.size();
-        if(i==n)
-            return 0;
-        
-        if(j!=-1&&dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int c1=solve(a,i+1,j,dp);
-        int c2=0;
-        if(j==-1||a[j]<a[i])
-            c2=1+solve(a,i+1,i,dp);
-        
-        if(j!=-1)
-            dp[i][j]=max(c1,c2);
-        
-        return max(c1,c2);
-    }
-    
     int lengthOfLIS(vector<int>& a) {
-        int c=0,n=a.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        //return solve(a,0,-1,dp);
-        
-        for(int i=n-1;i>=0;i--)
+        vector<int> b;
+        b.push_back(a[0]);
+        int n=a.size();
+        for(int i=1;i<n;i++)
         {
-            for(int j=i-1;j>=-1;j--)
+            if(a[i]>b.back())
+                b.push_back(a[i]);
+            else
             {
-                int c1=dp[i+1][j+1];
-                int c2=0;
-                if(j==-1||a[j]<a[i])
-                    c2=1+dp[i+1][i+1];
-                dp[i][j+1]=max(c1,c2);
+                int x=lower_bound(b.begin(),b.end(),a[i])-b.begin();
+                b[x]=a[i];
             }
         }
         
-        return dp[0][0];
-        
+        return b.size();
     }
-    
-    
 };
