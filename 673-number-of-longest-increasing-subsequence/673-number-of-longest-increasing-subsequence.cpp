669d418bb3863a3047127ff206108cf937b54452
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-05-08 03:08:45 +0530

    Time: 382 ms (5.03%), Space: 13.3 MB (51.92%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -17,33 +17,14 @@ public:
                 else if(a[j]<a[i]&&f[j]+1==f[i])
                     w[i]+=w[j];
             }
-
-            //f[i]+=c;
             m=max(m,f[i]);
         }
         int ans=0;
-        // for(int i=0;i<n;i++)
-        //     cout<<f[i]<<" ";
-            
-        //cout<<endl;
-//         for(int i=0;i<n;i++)
-//         {
-//             int c=0;
-//             for(int j=i-1;j>=0;j--)
-//             {
-//                 if(f[j]==f[i]-1&&a[j]<a[i])
-//                     c+=w[j];
-//             }
-            
-//             w[i]=max(w[i],c);
-//             if(f[i]==m)
-//                 ans+=w[i];
-//         }
         
         for(int i=0;i<n;i++)
             if(f[i]==m)
                 ans+=w[i];
-        //     cout<<w[i]<<" ";
+        
         return ans;
     }
 };
