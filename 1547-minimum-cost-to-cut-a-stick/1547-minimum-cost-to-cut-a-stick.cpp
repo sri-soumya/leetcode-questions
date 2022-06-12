c0d1a83c1592986dd09167987093a80b3f9f21c6
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-12 12:43:11 +0530

    Time: 44 ms (96.93%), Space: 10.4 MB (19.19%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -29,9 +29,31 @@ public:
         a.insert(a.begin(),0);
         a.push_back(k);
         
-        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
         
-        return solve(a,1,n,dp);
+        //return solve(a,1,n,dp);
+        
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][i]=a[i+1]-a[i-1];
+        }
+        
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i+1;j<=n;j++)
+            {
+                int ans=INT_MAX;
+                for(int k=i;k<=j;k++)
+                {
+                    int x=dp[i][k-1]+dp[k+1][j];
+                    ans=min(ans,x);
+                }
+                
+                dp[i][j]=ans+a[j+1]-a[i-1];
+            }
+        }
+        
+        return dp[1][n];
         
         
     }
