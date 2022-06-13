616c46627ae05124b61a2652a5ede163747424dd
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-13 20:49:53 +0530

    Time: 764 ms (10.70%), Space: 6.5 MB (83.60%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -11,19 +11,23 @@ public:
     int solve(string &s,int i,int j,vector<int> &dp)
     {
         
-        if(i>=j)
+        if(i>j)
+            return -1;
+        
+        if(i==j)
             return 0;
         
         if(dp[i]!=-1)
             return dp[i];
         
         int ans=INT_MAX;
-        for(int k=i;k<j;k++)
+        for(int k=i;k<=j;k++)
         {
             if(ispal(s,i,k))
             {
                 int x=solve(s,k+1,j,dp)+1;
                 ans=min(ans,x);
+                //cout<<i<<" "<<j<<ans<<endl;
             }
         }
 
@@ -34,7 +38,7 @@ public:
         
         int n=s.length();
         vector<int> dp(n,-1);        
-        int a= solve(s,0,n,dp)-1;
+        int a= solve(s,0,n-1,dp);
         return a;
         
         
