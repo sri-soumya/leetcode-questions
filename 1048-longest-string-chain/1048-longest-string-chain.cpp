285970f135ad2e5dcf12e1511bf80b5c745da4c4
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-15 13:24:00 +0530

    Time: 2427 ms (5.04%), Space: 644.6 MB (5.04%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -6,92 +6,51 @@ public:
         return a.length()<b.length();
     }
     
-    int solve(vector<string> &a,int i,int j,vector<vector<int>> &dp)
+    bool check(string a,string b)
     {
-        int n=a.size();
-        if(i==n)
-            return 0;
+        int n=a.length(),m=b.length();
         
-        if(j!=-1&&dp[i][j]!=-1)
-            return dp[i][j];
+        if(m-n!=1)
+            return false;
         
-        int c1=0,c2=0;
-        c1=solve(a,i+1,j,dp);
-        bool f=true;
-        int y=0,x;
-        if(j==-1)
-            c2=solve(a,i+1,i,dp)+1;
-        else
+        int c=0,i=0,j=0;
+        
+        while(i<n&&j<m)
         {
-            int nn=a[j].length(),mm=a[i].length();
-            int x=0,y=0;
-            bool f=true;
-            while(x<nn&&y<mm)
+            if(a[i]==b[j])
             {
-                if(a[j][x]==a[i][y])
-                {
-                    x++;
-                    y++;
-                }
-                else
-                    y++;
+                i++;
+                j++;
             }
-            
-            if(x<nn&&y>=mm)
-                f=false;
-            
-            if(f&&((mm-nn)==1))
-                c2=1+solve(a,i+1,i,dp);
-            
-            dp[i][j]= max(c1,c2);
+            else
+            {
+                j++;
+                c++;
+            }
         }
         
-        
-        return max(c1,c2);
-        
+        return i==n&&j<=m;
         
     }
     
     int longestStrChain(vector<string>& a) {
+        
         int n=a.size();
         sort(a.begin(),a.end(),compare);
-        
-        //vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         vector<int> dp(n,1);
-        int m=1;
+        int ans=1;
         
-        for(int i=n-2;i>=0;i--)
+        for(int i=0;i<n;i++)
         {
-            int c=0;
-            for(int j=i+1;j<n;j++)
+            for(int j=i-1;j>=0;j--)
             {
-                int nn=a[j].length(),mm=a[i].length(),x=0,y=0;
-                bool f=true;
-                
-                while(x<nn&&y<mm)
-                {
-                    if(a[j][x]==a[i][y])
-                    {
-                        x++;
-                        y++;
-                    }
-                    
-                    else
-                        x++;
-                }
-                
-                if(y<mm&&x>=nn)
-                    f=false;
-                if(f&&((nn-mm)==1))
-                  c=max(c,dp[j]);  
+                if(check(a[j],a[i]))
+                    dp[i]=max(dp[i],dp[j]+1);
             }
             
-            dp[i]=c+1;
-            m=max(m,dp[i]);
+            ans=max(ans,dp[i]);
         }
         
-        return m;
-        
-        //return solve(a,0,-1,dp);
+        return ans;
     }
 };
