20605be34e154a8d8cc6bdf1e5aa325d8aa5e627
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-16 13:55:43 +0530

    Time: 49 ms (13.24%), Space: 13.3 MB (40.68%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -25,12 +25,28 @@ public:
         int n=s.length();
         map<string,int> mp;
         
-        vector<int> dp(n,-1);
-        
+        vector<int> dp(n+1,0);
+        dp[n]=1;
         for(int i=0;i<a.size();i++)
+        {
             mp[a[i]]++;
+        }
+        //return solve(s,mp,0,dp);
         
-        return solve(s,mp,0,dp);
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(mp.count(s.substr(i,j-i+1))&&dp[j+1])
+                {
+                    dp[i]=true;
+                    break;
+                }
+            }
+            
+        }
+        
+        return dp[0];
         
     }
 };
