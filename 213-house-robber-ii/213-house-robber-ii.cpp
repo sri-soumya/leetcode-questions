a259561a46f6061baa0f4c5dfa27885c888084fe
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-16 14:54:29 +0530

    Time: 0 ms (100.00%), Space: 8 MB (16.94%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     
+    
+    int isolve(vector<int> &a,int k,int n)
+    {
+        vector<int> dp(a.size()+2,0);
+        
+        for(int i=n-1;i>=k;i--)
+        {
+            dp[i]=max(a[i]+dp[i+2],dp[i+1]);
+        }
+        
+        return dp[k];
+    }
+    
     int solve(vector<int> &a,int i,int n,vector<int> &dp)
     {
         if(i>=n)
@@ -19,7 +32,9 @@ public:
             return a[0];
         vector<int> dp1(n,-1),dp2(n,-1);
         
-        return max(solve(a,0,n-1,dp1),solve(a,1,n,dp2));
+        //return max(solve(a,0,n-1,dp1),solve(a,1,n,dp2));
+        return max(isolve(a,0,n-1),isolve(a,1,n));
+        
         
     }
 };
