d415df2cf95eab5214743dfa223b40873f6d5063
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-16 13:51:38 +0530

    Time: 32 ms (27.10%), Space: 13.5 MB (33.36%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    bool solve(string &s,map<string,int> &mp,int i,vector<int> &dp)
+    {
+        int n=s.length();
+        if(i==n)
+            return true;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        bool f=false;
+        
+        for(int j=i;j<n;j++)
+        {
+            if(mp.count(s.substr(i,j-i+1))&&solve(s,mp,j+1,dp))
+                return dp[i]= true;
+        }
+        
+        return dp[i]= false;
+    }
+    
+    bool wordBreak(string s, vector<string>& a) {
+        int n=s.length();
+        map<string,int> mp;
+        
+        vector<int> dp(n,-1);
+        
+        for(int i=0;i<a.size();i++)
+            mp[a[i]]++;
+        
+        return solve(s,mp,0,dp);
+        
+    }
+};
