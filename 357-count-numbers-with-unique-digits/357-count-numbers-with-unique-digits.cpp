549b0d3625c82798d7b4d5568dfb55fc6c860dd8
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-19 13:15:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.79%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
         
-        //vector<int> dp(n+1,0);
-        //dp[0]=1;
-        int s=10,c=1,x=1;
+        int s=10,c=1;
         for(int i=1;i<=n;i++)
         {
-            
-            //dp[i]=s;
-            x=s;
+            int x=s;
             s+=(x-c)*(10-i);
             c=x;
             
         }
-        return x;        
+        return c;        
     }
 };
