0d4b3a84b3ba9c635f5c705f40a67319cf916ee2
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-20 14:10:26 +0530

    Time: 127 ms (46.04%), Space: 74 MB (22.77%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,149 @@
+class Node
+{
+    Node* a[26]={NULL};
+    bool f=false;
+    
+    public:
+    bool containsNode(int x)
+    {
+        return a[x]!=NULL;
+    }
+    
+    void putNode(int x,Node *t)
+    {
+        a[x]=t;
+    }
+    
+    Node* getNode(int x)
+    {
+        return a[x];
+    }
+    
+    void putEnd()
+    {
+        f=true;
+    }
+    
+    bool isEnd()
+    {
+        return f;
+    }
+    
+    int solve(Node* root)
+    {
+        int s=0,z=0;
+        
+        for(int i=0;i<26;i++)
+        {
+            if(!root->a[i])
+                continue;
+            int x=solve(root->a[i])+1;
+            s+=x;
+            z++;
+        }
+        
+        if(!z)
+            s++;
+        return s;
+    }
+    
+};
+
+class Trie {
+    
+Node* root;
+public:
+    Trie() {
+        
+        root=new Node();
+        
+    }
+    
+    int insert(string s) {
+        
+        int n=s.length(),a=0;
+        Node* t=root;
+        for(int i=0;i<n;i++)
+        {
+            if(!t->containsNode(s[i]-'a'))
+            {
+                t->putNode(s[i]-'a',new Node());
+                if(!a)
+                    a=n+1;
+            }
+            t=t->getNode(s[i]-'a');
+        }
+        
+        t->putEnd();
+            
+        return a;
+    }
+    
+    bool search(string s) {
+     
+        int n=s.length();
+        Node* t=root;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!t->containsNode(s[i]-'a'))
+                return false;
+            t=t->getNode(s[i]-'a');
+        }
+        
+        return t->isEnd();
+        
+    }
+    
+    bool startsWith(string s) {
+        
+        int n=s.length();
+        Node* t=root;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!t->containsNode(s[i]-'a'))
+                return false;
+            t=t->getNode(s[i]-'a');
+        }
+        
+        return true;
+        
+    }
+    
+    int solve()
+    {
+        return root->solve(root);
+    }
+    
+};
+
+class Solution {
+public:
+    
+    static bool compare(string a,string b)
+    {
+        return a.length()>b.length();
+    }
+    
+    int minimumLengthEncoding(vector<string>& a) {
+        
+        Trie ob;
+        
+        int s=0,n=a.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            reverse(a[i].begin(),a[i].end());
+        }
+        
+        sort(a.begin(),a.end(),compare);
+        for(int i=0;i<n;i++)
+        {
+            s+=ob.insert(a[i]);
+        }
+        int x=ob.solve();
+        return s;
+        
+    }
+};
