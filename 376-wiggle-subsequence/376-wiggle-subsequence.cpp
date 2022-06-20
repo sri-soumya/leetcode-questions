02afe8c20aca9c8d1c86ae90396d979f1015621b
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-20 15:14:18 +0530

    Time: 4 ms (56.04%), Space: 7.2 MB (29.51%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,74 +1,31 @@
 class Solution {
 public:
     
-    int solve(vector<int> a,int i,int in,int k,vector<vector<vector<int>>> &dp)
-    {
-        if(i==0)
-            return 0;
-        
-        if(dp[i][in][k]!=-1)
-            return dp[i][in][k];
-        
-        if(in==0)
-        {
-            return dp[i][in][k]= max(solve(a,i-1,i,k,dp)+1,solve(a,i-1,in,k,dp));
-        }
-        
-        int x=a[in-1]-a[i-1];
-        if(x<0)
-            x=1;
-        else if(x>0)
-            x=2;
-        
-        if(x==0)
-            return dp[i][in][k]= solve(a,i-1,in,k,dp);
-        
-        if(k==0)
-        {
-            int op1=solve(a,i-1,in,k,dp);
-            
-            if(x==1)
-               op1=max(op1,solve(a,i-1,i,2,dp)+1);
-            else
-                op1=max(op1,solve(a,i-1,i,1,dp)+1);
-            
-            return dp[i][in][k]= op1;
-        }
-        
-        if(x!=k)
-            return dp[i][in][k]= solve(a,i-1,in,k,dp);
-        
-        if(x==1)
-            return dp[i][in][k]= max(solve(a,i-1,in,k,dp),solve(a,i-1,i,2,dp)+1);
-        return dp[i][in][k]= max(solve(a,i-1,in,k,dp),solve(a,i-1,i,1,dp)+1);
-    }
-
-    
     int wiggleMaxLength(vector<int>& a) {
         int n=a.size();
-        
-        //vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n+1,vector<int>(3,-1)));
-        
-        //return solve(a,n,0,0,dp);
-        
         vector<pair<int,int>> dp(n,{1,1});
-        
-        int ans=1;
-        
         for(int i=1;i<n;i++)
         {
-            for(int j=i-1;j>=0;j--)
+            
+            if(a[i]>a[i-1])
             {
-                if(a[i]>a[j])
-                    dp[i].first=max(dp[i].first,dp[j].second+1);
-                 if(a[i]<a[j])
-                    dp[i].second=max(dp[i].second,dp[j].first+1);
+                dp[i].first=dp[i-1].second+1;
+                dp[i].second=dp[i-1].second;
             }
             
-            ans=max(ans,max(dp[i].first,dp[i].second));
+            else if(a[i]<a[i-1])
+            {
+                dp[i].first=dp[i-1].first;
+                dp[i].second=dp[i-1].first+1;
+                
+            }
+            
+            else
+                dp[i]=dp[i-1];
+            
         }
         
-        return ans;
+        return max(dp[n-1].first,dp[n-1].second);
         
     }
 };
