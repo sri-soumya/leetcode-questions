6eefcc5daea72d911cf053e90bf5b943d1cd44bf
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-22 20:13:15 +0530

    Time: 596 ms (21.88%), Space: 103.1 MB (5.14%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -15,7 +15,7 @@ public:
     
     int solve(vector<pair<int,int>> &b,int m,int n,int i, vector<vector<vector<int>>> &dp)
     {
-        if(i<0)
+        if(i==0)
             return 0;
         // if(m<0||n<0)
         //     return -1e7;
@@ -27,8 +27,8 @@ public:
         
         int op1=solve(b,m,n,i-1,dp);
         int op2=0;
-        if(m-b[i].first>=0&&n-b[i].second>=0)
-            op2=solve(b,m-b[i].first,n-b[i].second,i-1,dp)+1;
+        if(m-b[i-1].first>=0&&n-b[i-1].second>=0)
+            op2=solve(b,m-b[i-1].first,n-b[i-1].second,i-1,dp)+1;
         
         return dp[i][m][n]= max(op1,op2);
     }
@@ -37,14 +37,30 @@ public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         
         vector<pair<int,int>> a;
-        vector<vector<vector<int>>> dp(strs.size(),vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        vector<vector<vector<int>>> dp(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
         for(int i=0;i<strs.size();i++)
         {
             int x=count(strs[i]);
             a.push_back({x,strs[i].length()-x});
         }
         
-        return solve(a,m,n,strs.size()-1,dp);
+        return solve(a,m,n,strs.size(),dp);
+        int l=strs.size();
         
+        for(int i=1;i<=l;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                for(int k=0;k<=n;k++)
+                {
+                    int op1=dp[i-1][m][n],op2=0;
+                    if(j-a[i-1].first>=0&&k-a[i-1].second>=0)
+                        op2=dp[i-1][j-a[i-1].first][k-a[i-1].second]+1;
+                    dp[i][j][k]=max(op1,op2);
+                }
+            }
+        }
+        
+        return dp[l][m][n];
     }
 };
