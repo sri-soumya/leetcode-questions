c26cdb0bb140ea9aee14a2d36d4b81b2ab5115a9
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-22 20:16:14 +0530

    Time: 600 ms (21.56%), Space: 102.9 MB (6.28%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -17,10 +17,6 @@ public:
     {
         if(i==0)
             return 0;
-        // if(m<0||n<0)
-        //     return -1e7;
-        
-        //cout<<i<<" "<<m<<" "<<n<<endl;
         
         if(dp[i][m][n]!=-1)
             return dp[i][m][n];
@@ -37,14 +33,14 @@ public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         
         vector<pair<int,int>> a;
-        vector<vector<vector<int>>> dp(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        vector<vector<vector<int>>> dp(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
         for(int i=0;i<strs.size();i++)
         {
             int x=count(strs[i]);
             a.push_back({x,strs[i].length()-x});
         }
         
-        return solve(a,m,n,strs.size(),dp);
+        //return solve(a,m,n,strs.size(),dp);
         int l=strs.size();
         
         for(int i=1;i<=l;i++)
@@ -53,7 +49,7 @@ public:
             {
                 for(int k=0;k<=n;k++)
                 {
-                    int op1=dp[i-1][m][n],op2=0;
+                    int op1=dp[i-1][j][k],op2=0;
                     if(j-a[i-1].first>=0&&k-a[i-1].second>=0)
                         op2=dp[i-1][j-a[i-1].first][k-a[i-1].second]+1;
                     dp[i][j][k]=max(op1,op2);
