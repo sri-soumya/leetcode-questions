7e9aa993e0ef57891f7416c8ed0d1ccf666fd901
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-24 13:34:33 +0530

    Time: 99 ms (5.12%), Space: 14.8 MB (12.43%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -27,6 +27,41 @@ public:
         return true;
     }
     
+    bool bfs(vector<vector<int>>& e,vector<int> &v,int i)
+    {
+        
+        queue<pair<int,int>> q;
+        q.push({i,1});
+        
+        while(q.size())
+        {
+            int node=q.front().first,c=q.front().second;
+            //cout<<node<<" "<<c<<endl;
+            q.pop();
+            v[node]=c;
+            int z;
+            if(c==1)
+                z=2;
+            else
+                z=1;
+            
+            for(auto x:e[node])
+            {
+                if(!v[x])
+                    q.push({x,z});
+                else if(v[x]==c)
+                {
+                    //cout<<node<<" "<<x<<" "<<c<<endl;
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+            
+        
+    }
+    
     bool isBipartite(vector<vector<int>>& e) {
         
         int n=e.size();
@@ -35,9 +70,10 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(v[i])
+            if(v[i]!=0)
                 continue;
-            if(!dfs(e,v,i,1))
+            int c=bfs(e,v,i);
+            if(c==0)
                 return false;
         }
         
