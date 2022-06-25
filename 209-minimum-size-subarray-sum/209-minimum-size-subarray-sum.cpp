a8288b24a25c3c1037053474a570f1d59ca23f56
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-25 13:27:29 +0530

    Time: 56 ms (12.26%), Space: 29.9 MB (7.46%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minSubArrayLen(int t, vector<int>& a) {
+        
+        int n=a.size();
+        vector<long long int> p(n,0);
+        p[0]=a[0];
+        
+        for(int i=1;i<n;i++)
+            p[i]=a[i]+p[i-1];
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            if(p[i]<t)
+                continue;
+            if(p[i]==t)
+            {
+                ans=min(ans,i+1);
+                continue;
+            }
+            int x=lower_bound(p.begin(),p.end(),p[i]-t)-p.begin();
+            int d=i-x+1;
+            if(p[x]==p[i]-t)
+                d--;
+            ans=min(ans,d);
+        }
+        
+        if(ans==INT_MAX)
+            return 0;
+        return ans;
+        
+    }
+};
