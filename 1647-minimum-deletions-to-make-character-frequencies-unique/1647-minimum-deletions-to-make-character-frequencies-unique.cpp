8292330acb507ec41d73fa9e6f9a4bcd828e050d
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-28 11:43:53 +0530

    Time: 82 ms (59.59%), Space: 17.4 MB (37.46%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        
+        map<int,int> mp;
+        vector<int> a(26,0);
+        int n=s.length(),c=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            //mp[s[i]-'a']++;
+            a[s[i]-'a']++;
+        }
+        
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]==0)
+                continue;
+            mp[a[i]]++;
+        }
+        
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]==0||mp[a[i]]==1)
+                continue;
+            
+            int j;
+            for(j=a[i];j>=0;j--)
+            {
+                if(!mp.count(j))
+                    break;
+                
+            }
+            
+            mp[a[i]]--;
+            c+=a[i]-j;
+            if(j)
+                mp[j]++;
+        }
+        
+        return c;
+    }
+};
