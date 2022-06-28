2e18fbadf5b02c261575eee383c73e182a5309a9
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-29 01:32:48 +0530

    Time: 12 ms (98.65%), Space: 17.8 MB (93.42%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& a) {
+        
+        int n=a.size();
+        int s=(n*(n+1))/2;
+        
+        for(int i=0;i<n;i++)
+            s-=a[i];
+        
+        return s;
+        
+    }
+};
