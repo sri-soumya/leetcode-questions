ac360157db93fce033efada890bc457ac21b3f2f
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-06-29 14:05:34 +0530

    Added solution - LeetHub

---
+++ Palindrome String - GFG/palindrome-string.c
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int isPalindrome(char s[])
+{
+    // Your code goes here
+    int n=strlen(s);
+    
+    for(int i=0;i<=n/2;i++)
+    {
+        if(s[i]!=s[n-i-1])
+            return false;
+    }
+    
+    return true;
+}
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	int t;
+   	scanf("%d", &t);
+   	while(t--)
+   	{
+   		char s[100000];
+   		scanf("%s", s);
+
+
+   		printf("%d\n", isPalindrome(s));
+   	}
+
+    return 0;
+}  // } Driver Code Ends
