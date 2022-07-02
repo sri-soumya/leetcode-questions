0ffc862edf50b61e183f44aa930efa3e07629a5a
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-02 12:00:27 +0530

    Time: 47 ms (70.27%), Space: 21.5 MB (23.63%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,35 +1,20 @@
-#define ff first
-#define ss second
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& a) {
-        
+     
         int n=a.size();
-        
-        vector<pair<string,int>> strs;
-        
-        for(int i=0;i<n;i++)
-            strs.push_back({a[i],i});
-        
-        for(int i=0;i<n;i++)
-            sort(strs[i].ff.begin(),strs[i].ff.end());
-        
-        sort(strs.begin(),strs.end());
-        
+        vector<string> s=a;
+        map<string,vector<string>> mp;
         vector<vector<string>> ans;
-        int c=0;
-        ans.push_back({a[strs[0].ss]});
         
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            if(strs[i].ff==strs[i-1].ff)
-                ans[c].push_back(a[strs[i].ss]);
-            else
-            {
-                ans.push_back({a[strs[i].ss]});
-                c++;
-            }
+            sort(a[i].begin(),a[i].end());
+            mp[a[i]].push_back(s[i]);
         }
+        
+        for(auto it:mp)
+            ans.push_back(it.second);
         
         return ans;
         
