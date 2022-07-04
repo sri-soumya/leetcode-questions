726fe31eb8c83c5da9f54f6f8d96018e76642817
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-04 11:24:56 +0530

    Time: 33 ms (52.60%), Space: 17.9 MB (40.68%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -13,24 +13,18 @@ public:
             
             if(a[i]>a[i-1])
                 p[i]=p[i-1]+1;
-            //cout<<p[i]<<" ";
         }
         
         
         
         int s=0;
-        //cout<<endl;
         for(int i=n-2;i>=1;i--)
         {
             if(a[i]>a[i+1]&&p[i]<=p[i+1])
                 p[i]=p[i+1]+1;
-            //cout<<p[i]<<" ";
             s+=p[i];
         }
-        
-        // for(int i=1;i<n-1;i++)
-        //     cout<<p[i]<<" ";
-        // cout<<endl;
+
         return s;
         
     }
