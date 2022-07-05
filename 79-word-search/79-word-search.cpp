06bb1cb02707f24b43e2433a32ef174e1742fc7a
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-05 13:08:00 +0530

    Time: 1030 ms (16.79%), Space: 8 MB (73.57%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<char>>& a, string b,vector<vector<bool>> &v,int i,int j,int k)
+    bool dfs(vector<vector<char>>& a, string b,int i,int j,int k)
     {
         int m=a.size(),n=a[0].size();
         int l=b.length();
          if(k==l)
             return true;
-        if(i<0||j<0||i>=m||j>=n||v[i][j])
+        if(i<0||j<0||i>=m||j>=n||a[i][j]=='#')
             return false;
        
         if(a[i][j]!=b[k])
             return false;
         
-        //cout<<i<<" "<<j<<" "<<k<<endl;
-        
-        
-        v[i][j]=true;
-        
-        bool f=dfs(a,b,v,i+1,j,k+1)||dfs(a,b,v,i-1,j,k+1)||dfs(a,b,v,i,j+1,k+1)||dfs(a,b,v,i,j-1,k+1);
-        v[i][j]=false;
-        
+        char c=a[i][j];
+        a[i][j]='#';
+        bool f=dfs(a,b,i+1,j,k+1)||dfs(a,b,i-1,j,k+1)||dfs(a,b,i,j+1,k+1)||dfs(a,b,i,j-1,k+1);
+        a[i][j]=c;
         return f;
     }
     
@@ -28,12 +24,12 @@ public:
         
         int m=a.size(),n=a[0].size();
         int l=b.length();
-        vector<vector<bool>> v(m,vector<bool>(n,0));
+        //vector<vector<bool>> v(m,vector<bool>(n,0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(a[i][j]==b[0]&&dfs(a,b,v,i,j,0))
+                if(a[i][j]==b[0]&&dfs(a,b,i,j,0))
                     return true;
             }
         }
