bf8e5d90d2272f01aaccee517d6df24c3c343e9a
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-06 14:39:11 +0530

    Time: 7 ms (81.74%), Space: 10.8 MB (59.97%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -23,8 +23,28 @@ public:
         
         // if(r==0&&c==0)
         //     return min(p,1);
-        vector<vector<double>> dp(r+1,vector<double>(c+1,-1));
-        return min(1.0,solve(p,r,c,dp));
+        vector<vector<double>> dp(r+1,vector<double>(c+1,0));
+        //return min(1.0,solve(p,r,c,dp));
+        
+        dp[0][0]=p;
+        
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=0;j<=c;j++)
+            {
+                double le=0.0,ri=0.0;
+                if(j-1>=0&&j-1<=i-1)
+                    le=dp[i-1][j-1];
+                if(j<=i-1)
+                    ri=dp[i-1][j];
+                le=max(0.0,(le-1)/2.0);
+                ri=max(0.0,(ri-1)/2.0);
+                
+                dp[i][j]=le+ri;
+            }
+        }
+        
+        return min(1.0,dp[r][c]);
         
     }
 };
