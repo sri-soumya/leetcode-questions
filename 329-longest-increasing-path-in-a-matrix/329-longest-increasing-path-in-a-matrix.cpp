4728b31ca841c73ba716a5dd1ee9eaab9b519b92
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-06 18:57:34 +0530

    Time: 85 ms (35.28%), Space: 16.3 MB (38.23%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,67 +1,44 @@
 class Solution {
 public:
     
-    /*1-up
-    2-down
-    3-left
-    4-right*/
-    
-    
-    int solve(vector<vector<int>>& a,int i,int j,int p,vector<vector<vector<int>>> &dp)
+    int solve(vector<vector<int>> &a,int i,int j,vector<vector<int>> &dp)
     {
-        int n=a.size(),m=a[0].size();
-        if(i<0||j<0||i>=n||j>=m)
+        int m=a.size(),n=a[0].size();
+        if(i<0||j<0||i>=m||j>=n)
             return 0;
-        //cout<<i<<" "<<j<<endl;
         
-        if(dp[i][j][p]!=-1)
-            return dp[i][j][p];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         
-        int x=INT_MIN;
-        if(p==1)
-            x=a[i-1][j];
-        else if(p==2)
-            x=a[i+1][j];
-        else if(p==3)
-            x=a[i][j-1];
-        else if(p==4)
-            x=a[i][j+1];
-        
-        if(x>=a[i][j])
-            return dp[i][j][p]= 0;
-        if(p==1)
-            return dp[i][j][p]= 1+max(solve(a,i,j+1,3,dp),max(solve(a,i,j-1,4,dp),solve(a,i+1,j,1,dp)));
-        else if(p==2)
-            return dp[i][j][p]= 1+max(solve(a,i,j+1,3,dp),max(solve(a,i,j-1,4,dp),solve(a,i-1,j,2,dp)));
-        else if(p==3)
-            return dp[i][j][p]= 1+max(solve(a,i,j+1,3,dp),max(solve(a,i-1,j,2,dp),solve(a,i+1,j,1,dp)));
-        else if(p==4)
-            return dp[i][j][p]= 1+max(solve(a,i-1,j,2,dp),max(solve(a,i,j-1,4,dp),solve(a,i+1,j,1,dp)));
-        else 
+        int dy[4]={-1,0,0,1};
+        int dx[4]={0,-1,1,0};
+        int ans=1;
+        for(int k=0;k<4;k++)
         {
-            
-            int op1=1+max(max(solve(a,i,j+1,3,dp),solve(a,i,j-1,4,dp)),max(solve(a,i+1,j,1,dp),solve(a,i-1,j,2,dp)));
-            int op2=0;
-            //op2=max(max(solve(a,i,j+1,0),solve(a,i,j-1,0)),max(solve(a,i+1,j,0),solve(a,i-1,j,0)));
-            return dp[i][j][p]= max(op1,op2);
+            if(i+dy[k]<0||i+dy[k]>=m||j+dx[k]<0||j+dx[k]>=n||a[i+dy[k]][j+dx[k]]<=a[i][j])
+                continue;
+            ans=max(ans,solve(a,i+dy[k],j+dx[k],dp)+1);
         }
-            
-            
+        
+        return dp[i][j]=ans;
+        
     }
     
     int longestIncreasingPath(vector<vector<int>>& a) {
-     
-        int n=a.size(),m=a[0].size();
-        //return solve(a,0,0,0);
-        int c=0;
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(5,-1)));
-        for(int i=0;i<n;i++)
+        
+        int m=a.size(),n=a[0].size();
+        
+        int ans=0;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
-                c=max(c,solve(a,i,j,0,dp));
+            for(int j=0;j<n;j++)
+            {
+                ans=max(ans,solve(a,i,j,dp));
+            }
         }
         
-        return c;
+        return ans;
         
     }
 };
