2fc8bd0d1818abfd2f67724a7ccd53eac225a71e
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-07 13:26:33 +0530

    Time: 51 ms (23.14%), Space: 27.9 MB (18.55%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     
-    bool solve(string s1,string s2,string s3,int i,int j,int k,vector<vector<vector<int>>> &dp)
+    bool solve(string s1,string s2,string s3,int i,int j,int k,vector<vector<int>> &dp)
     {
-        if(i==-1&&j==-1&&k==-1)
+        if(i==0&&j==0&&k==0)
             return true;
         
-        if(i>=0&&j>=0&&k>=0&&dp[i][j][k]!=-1)
-            return dp[i][j][k];
+        if(k==0&&(i||j))
+            return false;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         
         bool f1=0,f2=0;
-        if(i>=0&&k>=0&&s1[i]==s3[k])
+        if(i>0&&s1[i-1]==s3[k-1])
             f1=solve(s1,s2,s3,i-1,j,k-1,dp);
-        if(j>=0&&k>=0&&s2[j]==s3[k])
+        if(j>0&&s2[j-1]==s3[k-1])
             f2=solve(s1,s2,s3,i,j-1,k-1,dp);
         
-        if(i>=0&&j>=0&&k>=0)
-            dp[i][j][k]=f1||f2;
-        
-        return  f1||f2;
+        return dp[i][j]=f1||f2;
     }
     
     
@@ -26,28 +26,10 @@ public:
         
         int n=s1.length(),m=s2.length(),k=s3.length();
         
-//         while(k>=0)
-//         {
-//             cout<<n<<" "<<m<<" "<<k<<endl;
-//             if(n>=0&&s1[n]==s3[k])
-//             {
-//                 n--;
-//                 k--;
-//             }
-//             else if(m>=0&&s2[m]==s3[k])
-//             {
-//                 m--;
-//                 k--;
-//             }
-//             else
-//                 break;
-//         }
-        
-//         return n==-1&&m==-1&&k==-1;
-        
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(k,-1))); 
-        //vector<int> dp(k,-1);
-        return solve(s1,s2,s3,n-1,m-1,k-1,dp);
+       // vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(k,-1))); 
+        //vector<int> dp(m+1,-1);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(s1,s2,s3,n,m,k,dp);
         
      }
 };
