06c379cf8f081f4b9b2c744ca4ecdbe1efe91ad1
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-13 13:26:27 +0530

    Time: 22 ms (53.24%), Space: 13.1 MB (81.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,53 +1,22 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    void setZeroes(vector<vector<int>>& a) {
         
-//        int m=matrix.size();
-//         int n=matrix[0].size();
-//         unordered_map<int,int> a,b;
-//         for(int i=0;i<m;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {
-//                 if(matrix[i][j]==0)
-//                 {
-//                     a[i]++;
-//                     b[j]++;
-//                 }
-                
-//             }
-//         }
+        int m=a.size(),n=a[0].size();
         
-//         for(auto it=a.begin();it!=a.end();it++)
-//         {
-//             int x=it->first;
-            
-//             for(int i=0;i<n;i++)
-//                 matrix[x][i]=0;
-//         }
+        int c=1;
         
-//         for(auto it=b.begin();it!=b.end();it++)
-//         {
-//             int x=it->first;
-            
-//             for(int i=0;i<m;i++)
-//                 matrix[i][x]=0;
-//         }
-        
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int col=1;
         for(int i=0;i<m;i++)
         {
-            if(matrix[i][0]==0)
-                    col=0;
+            if(a[i][0]==0)
+                c=0;
+            
             for(int j=1;j<n;j++)
             {
-                
-                if(matrix[i][j]==0)
+                if(a[i][j]==0)
                 {
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    a[i][0]=0;
+                    a[0][j]=0;
                 }
             }
         }
@@ -56,12 +25,15 @@ public:
         {
             for(int j=n-1;j>=1;j--)
             {
-                if(matrix[i][0]==0||matrix[0][j]==0)
-                    matrix[i][j]=0;
+                if(a[i][0]==0||a[0][j]==0)
+                    a[i][j]=0;
             }
-            if(col==0)
-                matrix[i][0]=0;
         }
         
+        if(c==0)
+        {
+            for(int i=0;i<m;i++)
+                a[i][0]=0;
+        }
     }
 };
