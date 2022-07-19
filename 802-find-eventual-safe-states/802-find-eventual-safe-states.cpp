00eaa4299b1fa4b15ac196824366b60cb8f40c91
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-19 12:26:25 +0530

    Time: 239 ms (57.86%), Space: 47.1 MB (72.76%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     
-    bool solve(vector<vector<int>>& a,int i,vector<int> &b,vector<int> &v)
+    bool solve(vector<vector<int>>& a,int i,vector<int> &b)
     {
+        
         if(a[i].size()==0)
         {
             b[i]=1;
             return true;
         }
         
-        if(v[i])
-            return false;
-        //cout<<i<<endl;
         if(b[i]!=-1)
             return b[i];
         
-        v[i]=1;
-        
+        b[i]=0;
         bool f=true;
         
         for(auto x:a[i])
         {
-            if(!solve(a,x,b,v))
+            if(!solve(a,x,b))
             {
                 f=false;
                 break;
             }
         }
-        
-        v[i]=0;
         return b[i]=f;
     }
     
@@ -36,7 +31,7 @@ public:
         
         int n=a.size();
         vector<int> ans;
-        vector<int> v(n,0);
+        //vector<int> v(n,0);
         vector<int> b(n,-1);
         map<int,int> mp;
         
@@ -44,7 +39,7 @@ public:
         for(int i=0;i<n;i++)
         {
             if(b[i]==-1)
-                solve(a,i,b,v);
+                solve(a,i,b);
             if(b[i]==1)
                 ans.push_back(i);
         }
