760854802fa690e648958a8dc2a47d0af435b251
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-07-20 11:33:07 +0530

    Time: 704 ms (22.54%), Space: 49.8 MB (57.16%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& w) {
+        
+        int n=s.length(),m=w.size();
+        int c=0;
+        unordered_map<char,vector<int>> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]].push_back(i);
+        }
+        
+        for(auto x:w)
+        {
+            cout<<x<<endl;
+            int k=-1;
+            bool f=1;
+            for(auto j:x)
+            {
+                if(mp.count(j)==0)
+                {
+                    f=0;
+                    break;
+                }
+                int in=lower_bound(mp[j].begin(),mp[j].end(),k+1)-mp[j].begin();
+                if(in==mp[j].size())
+                {
+                    f=0;
+                    break;
+                }
+                //cout<<k+1<<" "<<in<<endl;
+                k=mp[j][in];
+            }
+            
+            if(f)
+                c++;
+        }
+        
+        return c;
+        
+    }
+};
