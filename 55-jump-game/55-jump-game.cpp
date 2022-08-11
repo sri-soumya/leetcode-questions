582fdc5bf280eaf99a71a7f64766a9df31cf12bb
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-08-11 14:14:58 +0530

    Time: 154 ms (26.33%), Space: 48.4 MB (44.36%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,50 +1,15 @@
 class Solution {
 public:
-    
-    bool solve(vector<int> &a,int i,vector<int> &dp)
-    {
-        int n=a.size();
-        
-        if(i==n-1)
-            return true;
-        
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        for(int j=1;j<=a[i];j++)
-        {
-            if(i+j>=n)
-                break;
-            //bool f=solve(a,i+j);
-            if(solve(a,i+j,dp))
-                return dp[i]= true;
-        }
-        
-        return dp[i]= false;
-    }
-    
-    bool canJump(vector<int>& a) {
-        int n=a.size();
-        
-        vector<int> dp(n,0);
-        
-        dp[n-1]=1;
+    bool canJump(vector<int>& nums) {
+     
+        int n=nums.size(),l=n-1;
         
         for(int i=n-2;i>=0;i--)
         {
-            for(int j=1;j<=a[i];j++)
-            {
-                if(dp[i+j])
-                {
-                    dp[i]=true;
-                    break;
-                }
-            }
+            if(i+nums[i]>=l)
+                l=i;
         }
         
-        return dp[0];
-        
-        //return solve(a,0,dp);
-        
+        return l==0;
     }
 };
