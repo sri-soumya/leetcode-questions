a35bb9982214eb4a352e26c1135c6522dcee1480
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-08-11 12:17:34 +0530

    Time: 78 ms (47.32%), Space: 18.8 MB (71.87%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,37 @@
+#define ll long long int
+class Solution {
+public:
+    
+    bool check(vector<int> &a,int m,int x)
+    {
+        ll c=0,n=a.size();
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%m)
+                c+=a[i]/m+1;
+            else
+                c+=a[i]/m;
+        }
+        
+        return c<=x;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        ll n=piles.size(),l=1,u=1e9,ans=0;
+        
+        while(l<=u)
+        {
+            int m=(l+u)/2;
+            if(check(piles,m,h))
+            {
+                ans=m;
+                u=m-1;
+            }
+            else
+                l=m+1;
+        }
+        
+        return ans;
+    }
+};
