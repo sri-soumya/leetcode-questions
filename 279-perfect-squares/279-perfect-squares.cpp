83a911f5782630a8cc26374673d27dc3dca7556c
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-08-17 18:53:01 +0530

    Time: 357 ms (38.29%), Space: 9.7 MB (29.29%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -12,47 +12,26 @@ public:
     {
         if(!n)
             return 0;
-        if(n<0)
-            return 10e7;
         
         if(dp[n]!=-1)
             return dp[n];
         
-        int a=10e7;
+        int ans=INT_MAX;
         
-        for(int i=n;i>=1;i--)
+        for(int i=1;i*i<=n;i++)
         {
-            if(!perfect(i))
-                continue;
-            a=min(a,1+solve(n-i,dp));
+            ans=min(ans,1+solve(n-i*i,dp));
         }
         
-        return dp[n]= a;
+        return dp[n]= ans;
     }
     
     int numSquares(int n) {
        
-        vector<int> dp(n+1,0);
+        vector<int> dp(n+1,-1);
         
-        //return solve(n,dp);
-        
-        for(int i=1;i<=n;i++)
-        {
-            int a=10e7;
-            if(perfect(i))
-            {
-                dp[i]=1;
-                continue;
-            }
-            for(int j=1;j*j<=i;j++)
-            {
-                a=min(a,1+dp[i-j*j]);
-            }
-            
-            dp[i]=a;
-        }
+        return solve(n,dp);
         
         
-        return dp[n];
     }
 };
