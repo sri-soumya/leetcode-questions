ac7c0db6923b2041f8458b4700b503802a4a2c96
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-08-24 21:26:43 +0530

    Time: 785 ms (31.42%), Space: 175.3 MB (8.01%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,30 +1,60 @@
 class NumArray {
 public:
-    vector<int> a;
-    int sum;
+    int n;
+    vector<int> st;
+    void build(int in,int l,int u,vector<int> &a,vector<int> &st)
+    {
+        if(l==u)
+        {
+            st[in]=a[l];
+            return;
+        }
+        int m=(l+u)/2;
+        build(in*2+1,l,m,a,st);
+        build(in*2+2,m+1,u,a,st);
+
+        st[in]=(st[in*2+1]+st[in*2+2]);
+    }
+
+    int query(int in,int l,int u,int x,int y,vector<int> &st)
+    {
+        if(y<l||x>u)
+            return 0;
+        if(l>=x&&u<=y)
+            return st[in];
+        int m=(l+u)/2;
+        return (query(in*2+1,l,m,x,y,st)+query(in*2+2,m+1,u,x,y,st));
+    }
+
+    void update(int in,int l,int u,int ind,int x,vector<int> &st)
+    {
+        if(l==u&&l==ind)
+        {
+            st[in]=x;
+            return;
+        }
+
+        int m=(l+u)/2;
+        if(ind<=m)
+            update(in*2+1,l,m,ind,x,st);
+        else
+            update(in*2+2,m+1,u,ind,x,st);
+        st[in]=(st[in*2+1]+st[in*2+2]);
+    }		
+    
     NumArray(vector<int>& nums) {
-        a=nums;
-        sum=0;
-        
-        for(auto x:a)
-            sum+=x;
+        n=nums.size();
+        st.resize(5*n);
+        build(0,0,n-1,nums,st);
     }
     
     void update(int index, int val) {
-        sum-=a[index];
-        a[index]=val;
-        sum+=val;
+        update(0,0,n-1,index,val,st);
     }
     
     int sumRange(int left, int right) {
-        int s=sum;
-        for(int i=0;i<left;i++)
-            s-=a[i];
-        
-        for(int i=right+1;i<a.size();i++)
-            s-=a[i];
-        
-        return s;
+        // return 0;
+        return query(0,0,n-1,left,right,st);
     }
 };
 
