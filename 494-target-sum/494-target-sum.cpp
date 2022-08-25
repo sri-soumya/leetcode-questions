df4df8b551130ee8c2f6844b58c6bfe524bed7d0
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-08-25 09:09:28 +0530

    Time: 631 ms (25.74%), Space: 62.5 MB (7.17%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,7 +1,8 @@
+#define pii pair<int,int>
 class Solution {
 public:
     
-    int solve(vector<int> &nums,int i,int t)
+    int solve(vector<int> &nums,int i,int t, map<pii,int> &dp)
     {
         int n=nums.size();
         
@@ -11,12 +12,17 @@ public:
                 return 1;
             return 0;
         }
-        return solve(nums,i+1,t+nums[i])+solve(nums,i+1,t-nums[i]);
+        
+        if(dp.count({i,t}))
+            return dp[{i,t}];
+        
+        return dp[{i,t}]= solve(nums,i+1,t+nums[i],dp)+solve(nums,i+1,t-nums[i],dp);
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return solve(nums,0,target);
+        map<pii,int> dp;
+        return solve(nums,0,target,dp);
         
     }
 };
