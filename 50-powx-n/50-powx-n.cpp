0493782731bccd6f60db976da34db89cfbf00f76
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-09-14 19:08:58 +0530

    Time: 9 ms (5.22%), Space: 5.9 MB (46.37%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,53 +3,19 @@ public:
     
     double solve(double x,int n)
     {
-        if(n==0)
-            return 1.0;
-        if(n<0)
-        {
-            x=1/x;
-            n=abs(n);
-        }
-        
+        if(!n)
+            return 1;
         double a=solve(x,n/2);
         a*=a;
-        
         if(n%2)
             a*=x;
-        
         return a;
     }
     
-    double myPow(double x, int m) {
-        
-        //return solve(x,n);
-        if(m==0)
-            return 1.0;
-        
-        long long n=abs(m);
-        
-        double ans=1.0;
-        
-        while(n>0)
-        {
-            if(n%2)
-            {
-                n--;
-                ans*=x;
-            }
-            
-            else
-            {
-                n/=2;
-                x*=x;
-            }
-        }
-        
-        if(m<0)
-            ans=1/ans;
-        
+    double myPow(double x, int n) {
+        double ans=solve(x,abs(n));
+        if(n<0)
+            return 1/ans;
         return ans;
-        
-        
     }
 };
