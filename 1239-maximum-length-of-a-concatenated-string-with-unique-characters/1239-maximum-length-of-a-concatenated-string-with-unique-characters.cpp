e547643e7ed37fd8985a5e63ce38c419c34ad59a
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-10-24 13:01:31 +0530

    Time: 129 ms (52.96%), Space: 50.1 MB (58.36%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    int freq(vector<int> &a)
+    {
+        int c=0;
+        for(auto x:a)
+        {
+            c+=x;
+        }
+        return c;
+    }
+    
+    int add(vector<int> &a,string &s)
+    {
+        map<int,int> mp;
+        for(auto x:s)
+        {
+            if(a[x-'a']>0||mp.count(x))
+                return -1;
+            mp[x]++;
+        }
+        
+        for(auto x:s)
+        {
+            a[x-'a']++;
+        }
+        
+        return 1;
+    }
+    
+    void sub(vector<int> &a,string &s)
+    {
+        for(auto x:s)
+        {
+            a[x-'a']--;
+        }
+    }
+    
+    
+    int solve(int i,vector<string> &arr,vector<int> &a)
+    {
+        
+        if(i<0)
+        {
+            return freq(a);
+        }
+        
+        int c1=0,c2=0;
+        
+        c1=solve(i-1,arr,a);
+        
+        int f=add(a,arr[i]);
+        if(f==-1)
+            return c1;
+        c2=solve(i-1,arr,a);
+        sub(a,arr[i]);
+        
+        return max(c1,c2);
+        
+    }
+    
+    int maxLength(vector<string>& arr) {
+        int n=arr.size();
+        vector<int> a(26);
+        return solve(n-1,arr,a);
+    }
+};
