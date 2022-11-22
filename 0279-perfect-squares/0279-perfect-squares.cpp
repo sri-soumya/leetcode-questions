c3dbe38720a3d69e19137df1d906df12486bf0d9
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2022-11-22 12:03:15 +0530

    Time: 426 ms (41.94%), Space: 9.7 MB (33.77%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    bool perfect(int n)
+    {
+        int x=ceil(sqrt(n));
+        return x*x==n;
+    }
+    
+    int solve(int n,vector<int> &dp)
+    {
+        if(!n)
+            return 0;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        int ans=INT_MAX;
+        
+        for(int i=1;i*i<=n;i++)
+        {
+            ans=min(ans,1+solve(n-i*i,dp));
+        }
+        
+        return dp[n]= ans;
+    }
+    
+    int numSquares(int n) {
+       
+        vector<int> dp(n+1,-1);
+        
+        return solve(n,dp);
+        
+        
+    }
+};
