4d8d02869146442c728b047d8d7818152cf3614e
Soumya Srivastava <69140198+sri-soumya@users.noreply.github.com>
2023-07-30 19:30:53 +0530

    Added solution - LeetHub

---
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    
+    bool ispal(string &s, int i, int j)
+    {
+        while(i<=j)
+        {
+            if(s[i++]!=s[j--]) return 0;
+        }
+        return 1;
+    }
+
+    int solve(string &s, int i, vector<int> &dp)
+    {
+        int n=s.size(),ans=INT_MAX;
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        for(int j=i;j<n;j++)
+        {
+            if(ispal(s,i,j))
+                ans=min(ans,solve(s,j+1,dp)+1);
+        }
+        
+        return dp[i]= ans;
+    }
+
+    int palindromicPartition(string str)
+    {
+        // code here
+        int n=str.size();
+        vector<int> dp(n,-1);
+        return solve(str,0,dp)-1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
